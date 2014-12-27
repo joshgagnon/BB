9ecbd17e6e8dc77a7e4409e73ce5efa78259b876
Joshua Gagnon <josh.n.gagnon@gmail.com>
2014-12-27 14:05:16 +1300

    Added first bunch of bs

---
+++ app/views/home.blade.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Redvale Ridge B&B</title>
+	<link href="{{ asset('bower/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+<body>
+
+<section class="container-fluid header">
+	<img class="logo" src="{{ asset('images/lawfirmstylefancy.png') }}" alt="Redvale Ridge B&B">
+</section>
+
+<section class="navigation ">
+<div class="container">
+	<a href="#about" class="btn">About the House</a>
+	<a href="#eco" class="btn">Eco Features</a>
+	<a href="#todo" class="btn">Things to do</a>
+	<a href="#location" class="btn">Location</a>
+</div>
+</section>
+
+<section id="about" class="container">
+<h2>About Us</h2>
+Located at the top of the South Island of New Zealand, our contemporary country home blah blah something about awarding winning Galeo Estate
+</section>
+
+<section id="eco" class="container">
+<h2>Eco Features</h2>
+<ul>
+<li>Passive solar, thermally and energy efficient design</li>
+<li>Thermally broken throughout the building envelope</li>
+<li>Hybrid systems which includes power and water automated management</li>
+<li>Solar panels for hot water and power</li>
+<li>Non toxic building materials</li>
+<li>Uprated natural wool insulation</li>
+<li>Low VOC paint and finishes</li>
+<li>Energy efficient lighting and appliances</li>
+<li>Recycled glass bottle carpet in bedrooms</li>
+</ul>
+</section>
+
+<section id="todo" class="container">
+<h2>Things To Do</h2>
+<ul>
+<li>The Great Taste Trail</li>
+<li>Wineries (25) and Wine Tours</li>
+<li>Beer and Craft Breweries (24)</li>
+<li>Southern Wilderness Gourmet Guided Walks</li>
+<li>Art/Craft/Pottery Trail</li>
+<li>Boating and Kayaking</li>
+<li>Abel Tasman Excursions</li>
+<li>Tramping</li>
+<li>Fishing</li>
+<li>Golfing</li>
+<li>Walks</li>
+<li>Horse Riding</li>
+<li>Beaches</li>
+</ul>
+</section>
+
+<section id="location" class="container"><!-- iframe src="https://www.google.com/maps/embed?pb=!1m22!1m12!1m3!1d1498.8526719733986!2d173.0710452869974!3d-41.2935169070187!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m7!1i0!3e6!4m0!4m3!3m2!1d-41.293762799999996!2d173.07104529999998!5e0!3m2!1sen!2snz!4v1419639257787" width="600" height="450" frameborder="0" style="border:0"></iframe> -->
+</section>
+
+<section id="footer" class="footer">
+<div class="container">
+<div class="nav-links">
+	<a href="#about" >About the House</a>
+	<a href="#eco" >Eco Features</a>
+	<a href="#todo" >Things to do</a>
+	<a href="#location" >Location</a>
+	<a href="#bookings" >Bookings</a>
+
+</div>
+<div class="contact-info">
+	Telephone - 
+	<a href="tel:+64035446478">+64 03 544 6478
+	</a>
+	Email -
+	<a href="mailto:mail@example.nz">
+    	mail@example.nz
+	</a>
+</div>
+<div class="copyright-info">
+		©Copyright 2015 Joshua Gagnon
+</div>
+</div>
+</section>
+</body>
+</html>
